3c766350bd92ff336317b6c329a0af53cff650df
Maowss <Maowss@gmail.com>
2019-09-18 19:19:08 -0300

    Initial commit

---
+++ app/src/main/java/br/com/etecia/appestrela/Nome_Activity.java
@@ -0,0 +1,14 @@
+package br.com.etecia.appestrela;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Nome_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.nome_layout);
+    }
+}
